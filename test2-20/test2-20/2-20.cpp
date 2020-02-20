9e999455be53f746223ea92c30c49c2cb94230a5
zhengyudong0430 <2238642137@qq.om>
2020-02-20 20:43:51 +0800

    素数打印

---
+++ test2-20/test2-20/2-20.cpp
@@ -0,0 +1,120 @@
+#include<stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	for (i = 100; i <= 200; i++)
+//	{
+//		int n = 0;
+//		for (n = 2; n < i; n++)
+//		{
+//		
+//			if (i%n == 0)
+//			{
+//				break;
+//			}
+//		}
+//		if (i == n)
+//		{
+//			printf("%d", i);
+//		}
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int i = 0;
+//	for (i = 100; i <= 200; i++)
+//	{
+//		int n = 0;
+//		for (n = 2; n <= i / 2; n++)
+//		{
+//			if (i%n == 0)
+//			{
+//				break;
+//			}
+//		}
+//		if (n > i / 2)
+//		{
+//			printf("%d", i);
+//		}
+//	}
+//	return 0;
+//}
+//#include<math.h>
+//int main()
+//{
+//	int i = 0;
+//	for (i = 100; i <= 200; i++)
+//	{
+//	
+//		int n = 0;
+//		for (n = 2; n <= sqrt((double)i); n++)
+//		{
+//			if (i%n == 0)
+//			{
+//				break;
+//			}
+//		}
+//		if (n > sqrt((double)i))
+//		{
+//			printf("%d", i);
+//		}
+//	}
+//
+//
+//	return 0;
+//}
+//#include<math.h>
+//int main()
+//{
+//	int i = 0;
+//	for (i = 101; i <= 200; i += 2)
+//	{
+//		int n = 0;
+//		for (n = 2; n <= sqrt((double)i); n++)
+//		{
+//		
+//			if (i%n == 0)
+//			{
+//				break;
+//			}
+//		}
+//		if (n > sqrt((double)i))
+//		{
+//
+//			printf("%d", i);
+//		}
+//	}
+//
+//	return 0;
+//}
+//写一个代码：打印1000年到2000年之间的闰
+
+//int main()
+//{
+//	int year = 0;
+//	for (year = 1000; year <= 2000; year++)
+//	{
+//	
+//		if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0))
+//		{
+//			printf("%d ", year);
+//		}
+//	
+//	}
+//
+//
+//
+//	return 0;
+//}
+//int main()
+//{
+//	int year = 0;
+//	for (year = 1000; year <= 2000; year++)
+//	{
+//	
+//	
+//	}
+//
+//	return 0;
+//}
