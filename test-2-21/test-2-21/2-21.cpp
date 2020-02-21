f21f0ac911f8b43c70f519f10a1a265248f1df26
zhengyudong0430 <2238642137@qq.com>
2020-02-22 01:29:27 +0800

    二分查找

--- test-2-21/test-2-21/2-21.cpp
+++ test-2-21/test-2-21/2-21.cpp
@@ -59,98 +59,37 @@
 //
 //	return 0;
 //}
-//编写代码在一个整形有序数组中查找具体的某个数
-//int main()
-//{
-//	int arr[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-//	int key = 0;
-//	scanf_s("%d", &key);
-//	int left = 0;
-//	int right = sizeof(arr) / sizeof(arr[0]) - 1;
-//	int mid = 0;
 //
-//	while (left <= right)
-//	{
-//		mid = (left + right) / 2;
-//		if (arr[mid] < key)
-//		{
-//			left = mid + 1;
-//		}
-//		else if (arr[mid]>key)
-//		{
-//			right = mid - 1;
-//		}
-//		else break;
-//	}
-//	if (left = right)
-//	{
-//		printf("找到了，下标是：%d", mid);
-//	}
-//	else
-//	{
-//		printf("没找到");
-//	}
-//	return 0;
-//}
 int main()
-
 {
-
-	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-
-	int left = 0;
-
-	int right = sizeof(arr) / sizeof(arr[0]) - 1;
-
+	int arr[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 	int key = 0;
 	scanf_s("%d", &key);
-
-	int mid = 0;
+	int left = 0;
+	int right = sizeof(arr) / sizeof(arr[0]) - 1;
+	
 
 	while (left <= right)
-
 	{
-
-		mid = (left + right) / 2;
-
-		if (arr[mid] > key)
-
+		int mid = (left + right) / 2;
+		if (arr[mid] < key)
 		{
-
-			right = mid - 1;
-
-		}
-
-		else if (arr[mid] < key)
-
-		{
-
 			left = mid + 1;
-
 		}
-
+		else if (arr[mid]>key)
+		{
+			right = mid - 1;
+		}
 		else
-
+		{
+			printf("找到了，下标是：%d", mid);
 			break;
-
+		}
 	}
-
-	if (left <= right)
-
+	if (left>right)
+	
 	{
-
-		printf("找到了，下标是%d\n", mid);
-
+		printf("没找到");
 	}
-
-	else
-
-	{
-
-		printf("没找到\n");
-
-	}
-
 	return 0;
-
 }
